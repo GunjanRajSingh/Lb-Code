1826eac1196b7d2912239f700781745a5ee0ad3f
GunjanRajSingh <52822461+GunjanRajSingh@users.noreply.github.com>
2025-09-14 23:15:26 +0530

    Version Updated

--- todoapp_infra/provider.tf
+++ todoapp_infra/provider.tf
@@ -5,9 +5,11 @@ terraform {
       version = "4.32.0"
     }
   }
+   required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
   features {}
   subscription_id = "cefa80e5-9af0-4e4a-9f43-e0a0491a5473"
+
 }
